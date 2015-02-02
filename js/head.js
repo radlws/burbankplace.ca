fbda28d7f58c60e35acbead6f4eb156015efaf76
Rad W <radzhome@gmail.com>
2015-02-01 22:42:57 -0500

    fixed up meta

--- js/head.js
+++ js/head.js
@@ -1,12 +1,6 @@
-//	contains title, bootstrap, meta, flexslider, modernizr, and custom css
+//	contains bootstrap, flexslider, modernizr, and custom css
 document.write(
-    '<meta content="charset=utf-8">'
-	+'<meta name="viewport" content="width=device-width, initial-scale=1.0">'
-    +'<meta name="author" content="Radomir Wojcik">'
-    +'<meta name="copyright" content="Copyright 2014 Radomir Wojcik radtek.ca. All rights reserved.">'
-    +'<meta name="description" content="Website for Burbank Place Apartments Limited Hamilton Cooperative Community Apartments near doowntown Hamilton.">'
-    +'<meta name="keywords" content="Co-operative Community; Hamilton co-op apartments; affordable living; affordable apartments; Hamilton apartments; pet-friendly community"> '
-	+'<link rel="stylesheet" href="css/bootstrap.css">'
+	'<link rel="stylesheet" href="css/bootstrap.css">'
 	+'<link rel="stylesheet" href="css/bootstrap-responsive.css">'
 	+'<link rel="stylesheet" href="css/lightbox.css">'
 	+'<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />'
@@ -18,4 +12,4 @@ document.write(
     +'<script type="text/javascript">var switchTo5x=true;</script>'
     +'<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>'
     +'<script type="text/javascript">stLight.options({publisher: "87d14b75-7d21-4e2d-91a4-6eb974f26cb3", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>'
-    );
+);
---
+++ js/meta.js
@@ -0,0 +1,8 @@
+document.write(
+    '<meta content="charset=utf-8">'
+	+'<meta name="viewport" content="width=device-width, initial-scale=1.0">'
+    +'<meta name="author" content="Radtek Inc.">'
+    +'<meta name="copyright" content="Copyright 2012-2015 Radtek.ca, All rights reserved.">'
+    +'<meta name="description" content="Burbank Place Apartments Limited Hamilton Cooperative Community Apartments near doowntown Hamilton.">'
+    +'<meta name="keywords" content="Co-operative Community; Hamilton co-op apartments; co-op apartment photos; affordable living; affordable apartments; Hamilton apartments; pet-friendly community">'
+);
