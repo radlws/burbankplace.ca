d8007c02574258dde6667aea7e2e203a86c9acf0
Rad W <radzhome@gmail.com>
2015-02-02 18:07:36 -0500

    added new photos

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,4 +1,4 @@
-for (i = 1; i < 21; i++) {
+for (i = 1; i < 38; i++) {
     document.write('<a class="example-image-link" href="img/photo_page/images/' + i + '.jpg" ' +
-    'data-lightbox="example-set"><img width="240" height="180" class="example-image" src="img/photo_page/thumbs/' + i + '.jpg" alt=""/></a>');
+    'data-lightbox="example-set"><img width="240" height="180" class="example-image" src="img/photo_page/thumbnails/' + i + '.jpg" alt=""/></a>');
 }
