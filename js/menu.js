4e258694e5950b12f4fe3216022790a3db1aa880
Rad W <radzhome@gmail.com>
2015-05-18 16:28:28 -0400

    added for sale

--- js/menu.js
+++ js/menu.js
@@ -15,12 +15,12 @@ document.write('<li>'
                 +'</li>'
 );
 
-/*
+
 document.write('<li>'
-                +'<a href="slideshow.html" class="menu-item">For Sale</a>'
+                +'<a href="for_sale.html" class="menu-item">For Sale</a>'
                 +'</li>'
 );
-*/
+
 document.write('<li>'
     +'<a href="about.html" class="menu-item">About Us</a>'
     +'</li>'
